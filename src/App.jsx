b8b869db4d53d0b028251149bb557f97fa8fc910
erDeep640390 <erdeep64930@gmail.com>
2023-09-07 22:15:47 +0530

    sidebar has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Header from'./components/Header'
+import Sidebar from './components/Sidebar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +11,7 @@ function App() {
     <div className="App">
        <Header/>
        <div className="bg-indigo-600 w-full h-screen"></div>
+       <Sidebar />
     </div>
   )
 }
